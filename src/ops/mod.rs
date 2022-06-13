eecb0d84dbacb46ad89c9ca4d399490d46d7cbe8
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-13 23:54:19 +0100

    Added error type as an associated type to the OperationImplementation trait

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,12 +1,11 @@
-use crate::tensor::InvalidTensorSizeError;
-
 trait OperationImplementation {
     type Input;
     type Output;
     type ElementType;
     type Initialised;
+    type ErrorType;
     fn init(
         self,
         iter: &mut dyn Iterator<Item = Self::ElementType>,
-    ) -> Result<Self::Initialised, InvalidTensorSizeError>;
+    ) -> Result<Self::Initialised, Self::ErrorType>;
 }
