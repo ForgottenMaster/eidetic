e0024f18948d13aec05ddc1e6bed2c6f610d53d3
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-23 17:32:21 +0100

    Clippy fix

--- src/tensors/try_construct_tensor.rs
+++ src/tensors/try_construct_tensor.rs
@@ -124,7 +124,7 @@ impl TryConstructTensor for (usize, usize, usize, usize, usize) {
 /// This is the error type which is used to report a failure to construct a new
 /// tensor from a provided iterator of elements.
 #[non_exhaustive]
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "thiserror", derive(Error))]
 pub enum TensorConstructionError {
     /// This variant is used in the case that the product of the specified tensor shape components (e.g. rows * columns for a rank 2 tensor) doesn't match the number
