b948808de86a52ed085e60ea6183311686f37de4
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-25 02:11:54 +0100

    Added the final typestate trait for operations that will allow optimisation. All traits should be finalised now, will start coding up the various operations next

---
+++ src/operations/backward/mod.rs
@@ -0,0 +1,15 @@
+//! This submodule contains the traits and types for representing the
+//! final stage of an operation in a training epoch. That of the operation
+//! having had the backward pass ran and ready for optimisation.
+
+use crate::private::Sealed;
+
+/// This trait represents the state of the operation after having the backward
+/// pass applied and is the final state of the operation. At this point if the
+/// instance is dropped then it's intended it doesn't update parameters, otherwise
+/// it can be applied to optimise the parameters with the calculated gradients.
+pub trait Operation: Sealed {
+    /// Function which consumes this instance and uses the built in optimiser
+    /// to update the parameters of the operation.
+    fn optimise(self);
+}
