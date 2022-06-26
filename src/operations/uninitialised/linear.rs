d3a7718e84b08518e4f8f1a89b61349571dea011
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-26 01:45:18 +0100

    Define the uninitialised form of the linear activation function. We'll operate under the assumption that it's expecting a 2 dimensional tensor like we will for most operations except those dealing with maybe convolutions later

--- src/operations/uninitialised/linear.rs
+++ src/operations/uninitialised/linear.rs
@@ -1,17 +1,44 @@
-//use crate::operations::uninitialised;
+use crate::operations::uninitialised;
 use crate::private::Sealed;
-use crate::tensors::rank::Rank;
-use crate::tensors::Tensor;
+use crate::Result;
 use core::marker::PhantomData;
 
-/// This operation represents a linear passthrough operation
-/// which is used for example as an activation function for a
-/// network layer. It can also be used as an initial input operation
-/// in order to have something to chain another operation against to
-/// ensure that input/output neurons are calculated.
-pub struct Linear<T, R: Rank>(PhantomData<Tensor<T, R>>);
+/// This operation represents a linear activation function on
+/// a rank 2 tensor.
+pub struct Linear<T>(PhantomData<T>, usize);
 
-impl<T, R: Rank> Sealed for Linear<T, R> {}
-/*impl<T, R: Rank> uninitialised::Operation for Linear<T, R> {
+impl<T> Linear<T> {
+    /// Function that constructs a new Linear operation given the
+    /// expected number of neurons (columns) of the data passing through
+    /// it.
+    #[must_use]
+    pub const fn new(neuron_count: usize) -> Self {
+        Self(PhantomData, neuron_count)
+    }
+}
+
+impl<T> Sealed for Linear<T> {}
+impl<T> uninitialised::Operation for Linear<T> {
     type Element = T;
-}*/
+    type Initialised = (); // TODO: replace this with something sensible
+
+    fn output_neuron_count(&self) -> usize {
+        self.1
+    }
+
+    fn with_iter_private(
+        self,
+        _iter: &mut impl Iterator<Item = Self::Element>,
+        _input_neuron_count: usize,
+    ) -> Result<Self::Initialised> {
+        Ok(()) // TODO: replace this with something sensible
+    }
+
+    fn with_seed_private(
+        self,
+        _seed: u64,
+        _input_neuron_count: usize,
+    ) -> Result<Self::Initialised> {
+        Ok(()) // TODO: replace this with something sensible
+    }
+}
