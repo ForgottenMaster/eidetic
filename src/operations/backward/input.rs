e6103804ca168475ab6bd066926f47a22403521a
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-29 17:24:53 +0100

    Added the final unit tests to get upto 100% coverage of the existing operations

--- src/operations/backward/input.rs
+++ src/operations/backward/input.rs
@@ -7,3 +7,17 @@ impl Sealed for Operation {}
 impl BackwardOperation for Operation {
     fn optimise(self) {}
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_optimise() {
+        // Arrange
+        let operation = Operation(());
+
+        // Act
+        operation.optimise();
+    }
+}
