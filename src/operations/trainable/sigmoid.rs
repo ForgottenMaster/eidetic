ca92c3499ad89c7802d18c212bf3474312ef0ac4
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-29 13:52:21 +0100

    Added unit tests for trainable state of the sigmoid operation

--- src/operations/trainable/sigmoid.rs
+++ src/operations/trainable/sigmoid.rs
@@ -33,3 +33,64 @@ impl trainable::Operation for Operation {
         Ok((self.construct(), clone))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::operations::TrainableOperation;
+
+    #[test]
+    fn test_into_initialised() {
+        // Arrange
+        let operation = Operation {
+            initialised: initialised::sigmoid::Operation { neurons: 42 },
+            last_output: Tensor::default(),
+        };
+        let expected = initialised::sigmoid::Operation { neurons: 42 };
+
+        // Act
+        let output = operation.into_initialised();
+
+        // Assert
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn test_forward_success() {
+        // Arrange
+        let mut operation = Operation {
+            initialised: initialised::sigmoid::Operation { neurons: 3 },
+            last_output: Tensor::default(),
+        };
+        let input = Tensor::<rank::Two>::new((1, 3), [-6.0, 0.0, 6.0]).unwrap();
+        #[cfg(feature = "f32")]
+        let expected = Tensor::<rank::Two>::new((1, 3), [0.002472623, 0.5, 0.9975274]).unwrap();
+        #[cfg(not(feature = "f32"))]
+        let expected =
+            Tensor::<rank::Two>::new((1, 3), [0.0024726231566347743, 0.5, 0.9975273768433653])
+                .unwrap();
+
+        // Act
+        let (_, output) = operation.forward(input).unwrap();
+
+        // Assert
+        assert_eq!(output, expected);
+        assert_eq!(operation.last_output, expected);
+    }
+
+    #[test]
+    fn test_forward_failure() {
+        // Arrange
+        let mut operation = Operation {
+            initialised: initialised::sigmoid::Operation { neurons: 2 },
+            last_output: Tensor::default(),
+        };
+        let input = Tensor::<rank::Two>::new((2, 3), [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]).unwrap();
+
+        // Act
+        let result = operation.forward(input);
+
+        // Assert
+        assert!(result.is_err());
+    }
+}
