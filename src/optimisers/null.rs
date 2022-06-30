5f67c1655bf363ccc261a900f7339416a12dc33d
Robin Firth <theforgottenmaster@googlemail.com>
2022-06-30 09:43:39 +0100

    Added code coverage hack

--- src/optimisers/null.rs
+++ src/optimisers/null.rs
@@ -33,6 +33,7 @@ impl<T> optimisers::base::OptimiserFactory<T> for OptimiserFactory {
 /// This struct is the concrete optimiser that is produced by the
 /// null `OptimiserFactory`.
 #[derive(Debug, Eq, PartialEq)]
+#[repr(C)] // hack to prevent this ZST being optimised out for code coverage reports
 pub struct Optimiser<T>(PhantomData<T>);
 
 impl<T> Sealed for Optimiser<T> {}
