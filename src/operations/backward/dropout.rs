f55d58e2df7e141397106e4866cdab7da8743681
Robin Firth <theforgottenmaster@googlemail.com>
2022-07-05 01:43:57 +0100

    Added a unit test for testing the optimise function on the dropout layer (that does nothing)

--- src/operations/backward/dropout.rs
+++ src/operations/backward/dropout.rs
@@ -11,3 +11,30 @@ impl<'a> Sealed for Operation<'a> {}
 impl<'a> BackwardOperation for Operation<'a> {
     fn optimise(self) {}
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::operations::{initialised, trainable};
+    use crate::tensors::{rank, Tensor};
+
+    #[test]
+    fn test_optimise() {
+        // Arrange
+        let mut backing = trainable::dropout::Operation {
+            initialised: initialised::dropout::Operation {
+                keep_probability: 0.8,
+                seed: None,
+            },
+        };
+        let backward = Operation {
+            _forward: forward::dropout::Operation {
+                _borrow: &mut backing,
+                mask: Tensor::<rank::Two>::new((1, 3), [1.0, 2.0, 3.0]).unwrap(),
+            },
+        };
+
+        // Act
+        backward.optimise();
+    }
+}
