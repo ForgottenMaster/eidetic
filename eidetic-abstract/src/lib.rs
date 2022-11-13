7508000aac66180401be7630154dd7705a59a178
Robin Firth <theforgottenmaster@googlemail.com>
2022-11-13 00:30:16 +0000

    Correct the workspace

--- eidetic-abstract/src/lib.rs
+++ eidetic-abstract/src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod tensor;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[test]
+fn test() {}
