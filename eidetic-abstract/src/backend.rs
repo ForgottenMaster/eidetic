e57cf1f84395d884df9678421ecb3f5a836da268
Robin Firth <theforgottenmaster@googlemail.com>
2022-11-14 10:56:32 +0000

    Added a Backend trait (currently empty)

---
+++ eidetic-abstract/src/backend.rs
@@ -0,0 +1,3 @@
+/// This trait defines the operations that must be supported by a given concrete backend
+/// such as ndarray or arrayfire in order to be usable by the Eidetic framework.
+pub trait Backend {}
--- eidetic-abstract/src/lib.rs
+++ eidetic-abstract/src/lib.rs
@@ -1,4 +1,15 @@
+//! This crate contains the abstract functionality for Eidetic which is independent of any specific
+//! concrete Backend. This defines the way that networks can be built and trained, but delegates the
+//! actual tensor storage and low-level operations used by layers to the backend currently in use.
+//!
+//! ```
+//! todo!("Sample of building a network");
+//! ```
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![deny(warnings, missing_docs, clippy::all)]
+
+mod backend;
 mod tensor;
 
-#[test]
-fn test() {}
+pub use backend::*;
