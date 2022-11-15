5fb9d249d64c5df0c861e3f0712480c09327cb25
Robin Firth <theforgottenmaster@googlemail.com>
2022-11-15 12:44:06 +0000

    Design decision change to make all Eidetic tensors rank 4 with the following form (batch index, layer index, row index, column index). This will simplify the operations we're performing as well as letting us use a single type for our tensor storage

--- eidetic-abstract/src/backend.rs
+++ eidetic-abstract/src/backend.rs
@@ -1,40 +1,22 @@
-use crate::{BackendDataType, Tensor0, Tensor1, Tensor2, Tensor3, Tensor4};
+use crate::{BackendDataType, Tensor};
 
 /// This trait defines the operations that must be supported by a given concrete backend
 /// such as ndarray or arrayfire in order to be usable by the Eidetic framework.
+///
+/// The Eidetic framework operations and layers will operate with the abstract Backend
+/// and Tensor traits but importantly callers should be able to rely on the results being the same
+/// between backends.
+///
+/// See [`Tensor`] for details about the data layout.
+///
+/// For different backends that support the same data storage type, the user should be free to
+/// select a different backend without changing any of the Eidetic framework, and the results should
+/// be identical.
 pub trait Backend {
-    /// The associated type to use when we want to represent a rank 0 tensor
-    /// AKA a scalar value.
+    /// The associated type to use when we want to represent our tensors.
+    /// Must implement Tensor for the data type we're storing.
     ///
     /// # Generics
     /// T is the underlying data type stored in the tensor. Must be supported by this backend.
-    type Tensor0<T: BackendDataType<Self>>: Tensor0<T>;
-
-    /// The associated type to use when we want to represent a rank 1 tensor
-    /// AKA a vector.
-    ///
-    /// # Generics
-    /// T is the underlying data type stored in the tensor. Must be supported by this backend.
-    type Tensor1<T: BackendDataType<Self>>: Tensor1<T>;
-
-    /// The associated type to use when we want to represent a rank 2 tensor
-    /// AKA a matrix.
-    ///
-    /// # Generics
-    /// T is the underlying data type stored in the tensor. Must be supported by this backend.
-    type Tensor2<T: BackendDataType<Self>>: Tensor2<T>;
-
-    /// The associated type to use when we want to represent a rank 3 tensor
-    /// AKA a vector of matrices.
-    ///
-    /// # Generics
-    /// T is the underlying data type stored in the tensor. Must be supported by this backend.
-    type Tensor3<T: BackendDataType<Self>>: Tensor3<T>;
-
-    /// The associated type to use when we want to represent a rank 4 tensor
-    /// AKA a matrix of matrices.
-    ///
-    /// # Generics
-    /// T is the underlying data type stored in the tensor. Must be supported by this backend.
-    type Tensor4<T: BackendDataType<Self>>: Tensor4<T>;
+    type Tensor<T: BackendDataType<Self>>: Tensor<T>;
 }
--- eidetic-abstract/src/tensor.rs
+++ eidetic-abstract/src/tensor.rs
@@ -1,36 +1,23 @@
-/// This trait should be implemented by a type that represents a
-/// rank 0 tensor, which is also known as a scalar value.
+/// This trait should be implemented by whichever type Eidetic should use
+/// to represent the tensors it is operating on. For simplicity Eidetic assumes
+/// all tensors to be rank 4 with the following structure (batch, layer, row, column/feature).
+///
+/// Therefore in order to access the value of the green channel (2nd channel) of the 9th pixel of the 4th row
+/// of the 5th image in the batch we can use an index such as:
+///
+/// (4, 1, 3, 8)
+///
+/// Where:
+/// - 4 => The index of the image in the batch
+/// - 1 => The index of the color channel we're accessing
+/// - 3 => The index of the row within the image
+/// - 8 => The index of the feature/column within the row
+///
+/// When converting external data into and out of Tensor form for calling code
+/// they can rely on this convention, and backends/implementations should arrange data
+/// in such a way as to be correct when it's retrieved (e.g. internally converting this row
+/// major form to column major).
 ///
 /// # Generics
 /// T is the underlying data type stored in the tensor.
-pub trait Tensor0<T> {}
-
-/// This trait should be implemented by a type that represents a
-/// rank 1 tensor, which is also known as a vector.
-///
-/// # Generics
-/// T is the underlying data type stored in the tensor.
-pub trait Tensor1<T> {}
-
-/// This trait should be implemented by a type that represents a
-/// rank 2 tensor, which is also known as a matrix.
-///
-/// # Generics
-/// T is the underlying data type stored in the tensor.
-pub trait Tensor2<T> {}
-
-/// This trait should be implemented by a type that represents a
-/// rank 3 tensor, which can be thought of as a vector of matrices
-/// or a 3-D array.
-///
-/// # Generics
-/// T is the underlying data type stored in the tensor.
-pub trait Tensor3<T> {}
-
-/// This trait should be implemented by a type that represents a
-/// rank 4 tensor, which can be thought of as a matrix of matrices
-/// or a 4-D array.
-///
-/// # Generics
-/// T is the underlying data type stored in the tensor.
-pub trait Tensor4<T> {}
+pub trait Tensor<T> {}
