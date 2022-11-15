1e267396b0034ed6ba32af27720af1334dfdf1b6
Robin Firth <theforgottenmaster@googlemail.com>
2022-11-15 15:19:52 +0000

    Collapse the derive and the error call into one cfg_attr macro

--- eidetic-abstract/src/error.rs
+++ eidetic-abstract/src/error.rs
@@ -3,9 +3,9 @@ use thiserror::Error;
 
 /// The enumeration containing the possible errors that Eidetic can produce.
 #[derive(Debug)]
-#[cfg_attr(feature = "thiserror", derive(Error))]
 #[cfg_attr(
     feature = "thiserror",
+    derive(Error), 
     error("An error that can occur during runtime operation of Eidetic.")
 )]
 pub enum Error {}
