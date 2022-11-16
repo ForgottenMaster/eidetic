b664ce47872c4b078aa1b603b31dc256fa6df6a1
Robin Firth <theforgottenmaster@googlemail.com>
2022-11-16 22:47:29 +0000

    Added an error type for tensor construction failure to use.

--- eidetic-abstract/src/error.rs
+++ eidetic-abstract/src/error.rs
@@ -3,12 +3,19 @@ use thiserror::Error;
 
 /// The enumeration containing the possible errors that Eidetic can produce.
 #[derive(Debug)]
-#[cfg_attr(
-    feature = "thiserror",
-    derive(Error), 
-    error("An error that can occur during runtime operation of Eidetic.")
-)]
-pub enum Error {}
+#[cfg_attr(feature = "thiserror", derive(Error))]
+pub enum Error {
+    /// This variant is used when the construction of a tensor could not be performed successfully due to having
+    /// insufficient elements.
+    #[cfg_attr(feature = "thiserror", error("Tensor could not be constructed. Requested shape was given as ({}, {}, {}, {}), but there were only {number_of_elements} elements.", requested_shape.0, requested_shape.1, requested_shape.2, requested_shape.3))]
+    TensorConstruction {
+        /// The shape that was requested for the rank 4 tensor.
+        requested_shape: (usize, usize, usize, usize),
+
+        /// The number of elements that were provided through the iterator given.
+        number_of_elements: usize,
+    },
+}
 
 /// A type alias which allows us to omit the error type when writing framework
 /// function signatures in order to ensure everything is using Eidetic's Error
